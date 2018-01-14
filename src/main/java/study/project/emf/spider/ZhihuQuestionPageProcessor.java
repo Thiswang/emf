b98480f9f281bcf93e087d6172b7c727f68cf59d
王哲 <wangzhe@wdai.com>
2018-01-14 20:55:40 +0800

    添加spider 界面渲染组件

---
+++ src/main/java/study/project/emf/spider/ZhihuQuestionPageProcessor.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2011-2018, 微贷网.
+ */
+package study.project.emf.spider;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * @author wangzhe 2018/1/14.
+ */
+public class ZhihuQuestionPageProcessor implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
